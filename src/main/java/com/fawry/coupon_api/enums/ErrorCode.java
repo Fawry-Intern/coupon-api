973f3e69245d8e18b3c95d586c9c4302c183bc3e
AhmedYahia74 <ahmedyahia0114666@gmail.com>
2025-03-17 02:50:19 +0200

    fix exception handling

--- src/main/java/com/fawry/coupon_api/enums/ErrorCode.java
+++ src/main/java/com/fawry/coupon_api/enums/ErrorCode.java
@@ -5,17 +5,8 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not found"),
-    RESOURCE_ALREADY_EXISTS(HttpStatus.CONFLICT, "Resource already exists"),
-
-
-    INSUFFICIENT_STOCK(HttpStatus.BAD_REQUEST, "Insufficient stock for requested quantity"),
-    NEGATIVE_STOCK_QUANTITY(HttpStatus.BAD_REQUEST, "Stock quantity cannot be negative"),
-
-
-    CONSUMPTION_NOT_FOUND(HttpStatus.NOT_FOUND, "Product consumption not found"),
-    CONSUMPTION_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to create product consumption"),
-
+    COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "Coupon not found"),
+    COUPON_ALREADY_EXISTS(HttpStatus.CONFLICT, "Coupon already exists"),
 
     VALIDATION_ERROR(HttpStatus.BAD_REQUEST, "Validation failed"),
 
